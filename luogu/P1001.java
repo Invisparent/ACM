c7529948afa5f68caf3fdabeeb13408c4046f5c5
Invisparent <invisparent@outlook.com>
2023-03-22 17:43:59 +0800

    Create P1001.java

---
+++ luogu/P1001.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ivsScan = new Scanner(System.in);
+        int a = ivsScan.nextInt();
+        int b = ivsScan.nextInt();
+        System.out.println(a + b);
+    }
+}
